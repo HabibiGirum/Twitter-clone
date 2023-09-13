ba49444072e9e6805943f48a32dfd79c12381aaa
Habtamu <habtamugirum0489@gmail.com>
2023-09-13 21:40:26 +0300

    admin

--- members/models.py
+++ members/models.py
@@ -5,3 +5,6 @@ class Members(models.Model):
     lastname = models.CharField(max_length=30)
     phone = models.IntegerField(null= True)
     joined_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
